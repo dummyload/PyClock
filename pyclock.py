a8d8916f7ae6af3947083d8958e20775e4d7f394
Tom Winn <thomas.winn@ericsson.com>
2016-11-15 08:26:25 +0000

    Updated clock: It works!

--- pyclock.py
+++ pyclock.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python
 
 
+"""
+Analogue clock using PyGTK2.
+"""
+
+
+from datetime import datetime
+from gtk.gdk import Rectangle
+from gobject import timeout_add
 from math import cos, pi, sin
 import gtk
 
 
 class PyClock(gtk.Window):
     """
-
+    Main class for PyClock.
     """
+
+
 
     def __init__(self):
         """
@@ -21,10 +31,15 @@ class PyClock(gtk.Window):
         self.set_position(gtk.WIN_POS_CENTER)
         self.connect("destroy", gtk.main_quit)
 
+        self._time = None
+        self._update()
+
         self._draw_area = gtk.DrawingArea()
 
         self._draw_area.connect("expose-event", self._expose)
         self.add(self._draw_area)
+
+        timeout_add(1000, self._update)
 
         self.show_all()
 
@@ -33,10 +48,23 @@ class PyClock(gtk.Window):
 
         """
         self._context = self._draw_area.window.cairo_create()
-        self._context.rectangle(0, 0, self.allocation.width, self.allocation.height)
+        self._context.rectangle(0, 0, self.allocation.width,
+                                self.allocation.height)
         self._context.clip()
 
+        dimensions = self.get_allocation()
+
+        # get the center point of the window.
+        self._center_x = dimensions.x + dimensions.width / 2
+        self._center_y = dimensions.y + dimensions.height / 2
+
+        # ensure the circle remains a circle when resizing the window,
+        # with about a 5 pixel padding from any edge.
+        self._radius = min(dimensions.width /2,
+                           dimensions.height / 2) - 5
+
         self._draw_clock_face()
+        self._draw_hands()
 
         return False
 
@@ -44,18 +72,10 @@ class PyClock(gtk.Window):
         """
         Draw the clock face.
         """
-        dimensions = self.get_allocation()
-
-        # get the centre point of the window.
-        center_x = dimensions.x + dimensions.width / 2
-        center_y = dimensions.y + dimensions.height / 2
-
-        # ensure the circle stays, well, a circle!
-        radius = min(dimensions.width / 2,
-                     dimensions.height / 2) - 5
-
-        # clock back
-        self._context.arc(center_x, center_y, radius, 0, 2 * pi)
+        # clock background
+        #
+        self._context.arc(self._center_x, self._center_y, self._radius,
+                          0, 2 * pi)
         # main circle
         self._context.set_source_rgb(1, 1, 1)
         self._context.fill_preserve()
@@ -67,18 +87,122 @@ class PyClock(gtk.Window):
         for tick in range(12):
             self._context.save()
 
+            # if the tick falls on a quarter, extend the line slightly.
             if tick % 3 == 0:
-                inset = 0.2 * radius
+                # accentuate the quarter lines by making them slightly
+                # wider (thicker) than the others and changing the
+                # colour of the line.
+                self._context.set_line_width(4)
+                self._context.set_source_rgb(0.7, 0.2, 0.0)
+                inset = 0.2 * self._radius
             else:
-                inset = 0.1 * radius
+                inset = 0.1 * self._radius
 
-            self._context.move_to(center_x + (radius - inset) * cos(tick * pi / 6),
-                                  center_y + (radius - inset) * sin(tick * pi / 6))
-            self._context.line_to(center_x + radius * cos(tick * pi / 6),
-                                  center_y + radius * sin(tick * pi / 6))
+            self._context.move_to(self._center_x + (self._radius - inset) * cos(tick * pi / 6),
+                                  self._center_y + (self._radius - inset) * sin(tick * pi / 6))
+            self._context.line_to(self._center_x + self._radius * cos(tick * pi / 6),
+                                  self._center_y + self._radius * sin(tick * pi / 6))
             self._context.stroke()
             self._context.restore()
 
+    def _draw_hands(self):
+        """
+        Draw the hands of the clock.
+        """
+        # get the hour, minute and second from the time instance
+        # variable.
+        self._draw_hour_hand()
+        self._draw_minute_hand()
+        self._draw_second_hand()
+
+    def _draw_hour_hand(self):
+        """
+        Draw the hour hand.
+        """
+        hour = self._time.hour
+        minute = self._time.minute
+
+        # the hour hand is rotated 30 degrees (pi/6 r) per hour + 1/2 a
+        # degree (pi/360) per minute.
+        self._context.save()
+        self._context.set_line_width(2.5 * self._context.get_line_width())
+
+        # move cursor to the center of the drawing area in preparation
+        # to draw the hour hand.
+        self._context.move_to(self._center_x, self._center_y)
+        # draw the how hand.
+        self._context.line_to(
+            self._center_x + self._radius / 2 * sin(pi /6 * hour + pi / 360 * minute),
+            self._center_y + self._radius / 2 * -cos(pi /6 * hour + pi / 360 * minute))
+        self._context.stroke()
+        self._context.restore()
+
+    def _draw_minute_hand(self):
+        """
+        Draw the minute hand.
+        """
+        minute = self._time.minute
+
+        # the minute hand is rotated 6 degrees (pi/30 r) per minute.
+        self._context.save()
+        self._context.move_to(self._center_x, self._center_y)
+
+        self._context.line_to(self._center_x + self._radius * 0.75 * sin(pi / 30 * minute),
+                              self._center_y + self._radius * 0.75 * -cos(pi / 30 * minute))
+
+        self._context.stroke()
+        self._context.restore()
+
+    def _draw_second_hand(self):
+        """
+        Draw the second hand.
+        """
+        second = self._time.second
+
+        # operates similar to the second hand:
+        # rotated 6 degrees (pi/30 r) per minute.
+        self._context.save()
+
+        # differentiate the second hand from the minute hand by
+        # changing the colour of the line.
+        self._context.set_source_rgb(1.0, 0.0, 0.0)
+
+        self._context.move_to(self._center_x, self._center_y)
+
+        self._context.line_to(self._center_x + self._radius * 0.75 * sin(pi / 30 * second),
+                              self._center_y + self._radius * 0.75 * -cos(pi / 30 * second))
+
+        self._context.stroke()
+        self._context.restore()
+
+    def redraw_canvas(self):
+        """
+        Redraw the canvas to make it look as thought the hands are
+        ticking.
+        """
+        if self.window:
+            dimensions = self.get_allocation()
+            rect = Rectangle(0, 0, dimensions.width, dimensions.height)
+            self.window.invalidate_rect(rect, True)
+            self.window.process_updates(True)
+
+    def _update(self):
+        """
+        Get the current time of the machine.
+        """
+        self._TIME = datetime.now()
+
+        # returning True ensures that the timer object will fire again.
+        return True
+
+    def _get_time(self):
+        return self._time
+
+    def _set_time(self, the_time):
+        self._time = the_time
+        self.redraw_canvas()
+
+    _TIME = property(_get_time, _set_time)
 
 if __name__ == "__main__":
     PyClock()
